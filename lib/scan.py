ad48a42864d9106e816693c772d2e6cd6983c71f
Shigechika AIKAWA <aikawa.shigechika@nihon-u.ac.jp>
2021-10-12 16:19:55 +0900

    By removing the ipaddress check, possible to access via hostname as well.

--- lib/scan.py
+++ lib/scan.py
@@ -2,8 +2,7 @@ import logging
 import traceback
 
 def getSnmpString(addr, ver, comm):
-    from ipaddress import IPv4Address
-    import subprocess, ipaddress, re
+    import subprocess, re
 
     logging.debug("[call] scan/getCPE")
     logging.debug("  addr is: %s", addr)
@@ -12,24 +11,6 @@ def getSnmpString(addr, ver, comm):
 
     snmpSysDescr = ".1.3.6.1.2.1.1.1.0"
     snmpSysName = ".1.3.6.1.2.1.1.5.0"
-
-    # check ipaddress data
-    try:
-        chkaddr = ipaddress.IPv4Address(addr)
-    except:
-        logging.error(" Incorrect IP Address: %s", addr)
-        exit(1)
-
-    if chkaddr.is_multicast or chkaddr.is_global or chkaddr.is_loopback or chkaddr.is_unspecified or chkaddr.is_reserved:
-        logging.error("Bad IP Address: %s", addr)
-        logging.error("  is_multicast: %s", chkaddr.is_multicast)
-        logging.error("  is_global: %s", chkaddr.is_global)
-        logging.error("  is_loopback: %s", chkaddr.is_loopback)
-        logging.error("  is_unspecified: %s", chkaddr.is_unspecified)
-        logging.error("  is_reserved: %s", chkaddr.is_reserved)
-        exit(1)
-
-    logging.debug("IPAddress check is ok")
 
     walkstring = 'snmpget -v ' + str(ver) + ' -c ' + comm + ' ' + addr
     logging.debug("snmpgetting: %s", walkstring)
