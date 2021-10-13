1583dcc41b1763c1bcdc7c6d19500eac2b1d4cb7
Shigechika AIKAWA <aikawa.shigechika@nihon-u.ac.jp>
2021-10-14 08:28:51 +0900

    feat: support Cisco IOS-XE and Arista EOS

--- lib/cpe.py
+++ lib/cpe.py
@@ -4,7 +4,14 @@ import re
 def getCPEName(sysDescr):
     # CPE 2.3 like "cpe:2.3:o:cisco:ios:14.1\(2\)s2:*:*:*:*:*:*:*""
     
-    if re.match(r'Cisco IOS Software', sysDescr):
+    if re.match(r'Cisco IOS Software.*IOS-?XE', sysDescr):
+        # IOS-XE regexp must evaluate before IOS
+        sysDescr = sysDescr.replace('\n', '')
+        verAll = sysDescr.split()[sysDescr.split().index("Version") + 1]
+        verMajor = re.findall(r'^.*[^,]', verAll)[0]
+        ret = f"cpe:2.3:o:cisco:ios_xe:{verMajor.lower()}:*:*:*:*:*:*:*"
+        return ret
+    elif re.match(r'Cisco IOS Software', sysDescr):
         # get version string "15.1(4)M3,"
         # Version A.B(C)D
         #   major release: A.B(Train:15.1)
@@ -30,6 +37,11 @@ def getCPEName(sysDescr):
         verMajor = re.findall(r'^[0-9]*.*R', verAll)[0][0:-1]
         verMaint = re.findall(r'R[0-9]*.*,', verAll)[0][0:-1]
         ret = f"cpe:2.3:o:juniper:junos:{verMajor}:{verMaint.lower()}:*:*:*:*:*:*"
+        return ret
+    elif re.match(r'Arista Networks EOS', sysDescr):
+        verAll = sysDescr.split()[sysDescr.split().index("version") + 1]
+        verMajor = re.findall(r'^[0-9].*', verAll)[0]
+        ret = f"cpe:2.3:o:arista:eos:{verMajor.lower()}:*:*:*:*:*:*:*"
         return ret
     else:
         logging.info(
--- lib/test_cpe.py
+++ lib/test_cpe.py
@@ -23,6 +23,18 @@ class TestgetCPEName(unittest.TestCase):
         CPE = "cpe:2.3:o:cisco:ios:12.2\(17d\)SXB11:*:*:*:*:*:*:*"
         self.assertEqual(ret, CPE)
 
+    def test_getCPENameCisco04(self):
+        sysDescr = "Cisco IOS Software [Gibraltar], Catalyst L3 Switch Software (CAT9K_LITE_IOSXE), Version 16.12.4, RELEASE SOFTWARE (fc5)\nTechnical Support: http://www.cisco.com/techsupport\nCopyright (c) 1986-2020 by Cisco Systems, Inc.\nCompiled Thu 09-Jul-20 19:31 by m"
+        ret = cpe.getCPEName(sysDescr)
+        CPE = "cpe:2.3:o:cisco:ios_xe:16.12.4:*:*:*:*:*:*:*"
+        self.assertEqual(ret, CPE)
+
+    def test_getCPENameCisco05(self):
+        sysDescr = "Cisco IOS Software, IOS-XE Software, Catalyst 4500 L3 Switch Software (cat4500es8-UNIVERSALK9-M), Version 03.06.07.E RELEASE SOFTWARE (fc3)\nTechnical Support: http://www.cisco.com/techsupport\nCopyright (c) 1986-2017 by Cisco Systems, Inc.\nCompiled Wed"
+        ret = cpe.getCPEName(sysDescr)
+        CPE = "cpe:2.3:o:cisco:ios_xe:03.06.07.e:*:*:*:*:*:*:*"
+        self.assertEqual(ret, CPE)
+
     def test_getCPENameJuniper01(self):
         sysDescr = "Juniper Networks, Inc. mx240 internet router, kernel JUNOS 18.4R3-S7.2, Build date: 2021-02-03 13:31:24 UTC Copyright (c) 1996-2021 Juniper Networks, Inc."
         ret = cpe.getCPEName(sysDescr)
@@ -33,6 +45,12 @@ class TestgetCPEName(unittest.TestCase):
         sysDescr = "Juniper Networks, Inc. ex4300-32f Ethernet Switch, kernel JUNOS 20.4R2-S2.2, Build date: 2021-08-12 00:07:44 UTC Copyright (c) 1996-2021 Juniper Networks, Inc."
         ret = cpe.getCPEName(sysDescr)
         CPE = "cpe:2.3:o:juniper:junos:20.4:r2-s2.2:*:*:*:*:*:*"
+        self.assertEqual(ret, CPE)
+
+    def test_getCPENameArista(self):
+        sysDescr = "Arista Networks EOS version 4.23.0F running on an Arista Networks DCS-7050TX-64"
+        ret = cpe.getCPEName(sysDescr)
+        CPE = "cpe:2.3:o:arista:eos:4.23.0f:*:*:*:*:*:*:*"
         self.assertEqual(ret, CPE)
         
 if __name__ == "__main__":
